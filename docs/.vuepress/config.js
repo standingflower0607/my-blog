87801b44b67ca6143ac8b9699aed6343aa05cf86
立花冠 <tachibanakan@tachibanakannoMacBook-Pro.local>
2020-04-02 03:11:19 +0900

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -70,29 +70,16 @@ module.exports = {
         "text": "Tag"
       }
     },
-    "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
-    ],
     "logo": "/logo.png",
     "search": true,
     "searchMaxSuggestions": 10,
+    "smoothScroll": true,
     "sidebar": "auto",
     "lastUpdated": "Last Updated",
     "author": "Kan Tachibana",
-    "authorAvatar": "/avatar.png",
+    "authorAvatar": "/for_insta.jpg",
     "record": "xxxx",
-    "startYear": "2017"
+    "startYear": "2020"
   },
   "markdown": {
     "lineNumbers": true
--- docs/views/category1/2019/test.md
+++ docs/views/category1/2019/test.md
@@ -1,10 +1,48 @@
 ---
-title: second page in category1
-date: 2019-09-21
+title: categories and tags test
+date: 2019-04-02
 tags:
- - tag2
+ - javascript
+ - python 
+ - typescript
 categories:
- - category1
+ - frontend
+ - backend
 ---
 
-#This is a test
+# This is a test
+
+---
+:tada: :100:
+[[toc]]
+
+# boxes 
+
+::: tip
+This is a tip
+:::
+
+::: warning
+This is a warning
+:::
+
+::: danger
+This is a dangerous warning
+:::
+
+::: details
+This is a details block, which does not work in IE / Edge
+:::
+
+# code
+
+``` html
+<ul>
+  <li
+    v-for="todo in todos"
+    :key="todo.id"
+  >
+    {{ todo.text }}
+  </li>
+</ul>
+```
